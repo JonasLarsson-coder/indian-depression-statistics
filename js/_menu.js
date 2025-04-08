a0608753cdb7adcab73189a5a53f7b6ae1aab331
JonasLarsson-coder <Jonas.larsson.data24sto@edu.tucsweden.se>
2025-04-08 21:01:15 +0200

    added headlines and data

--- js/_menu.js
+++ js/_menu.js
@@ -1,6 +1,6 @@
 createMenu('Indian depression statistics', [
   { name: 'Average Age', script: 'avgAge.js' },
-  { name: 'Visa ett år', script: 'one-year.js' },
-  { name: 'Jämför två år', script: 'compare-two-years.js' },
-  { name: 'Hitta trender', script: 'trends.js' }
+  { name: 'Suicidal', script: 'suicidalThoughtsAndDepression.js' },
+  { name: 'Sleep', script: 'sleepAndDepressionDiagram.js' },
+  { name: 'Mentall Illnes', script: 'historyOfMentallIllnesAndDepression.js' }
 ]);
