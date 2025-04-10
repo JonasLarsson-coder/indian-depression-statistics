973e942b445e0e6023af55cf1c3d7decf482ba45
JonasLarsson-coder <Jonas.larsson.data24sto@edu.tucsweden.se>
2025-04-10 10:33:36 +0200

    addded .DS_Store to gitignore folder

--- js/avgAge.js
+++ js/avgAge.js
@@ -7,7 +7,7 @@ import makeChartFriendly from './libs/makeChartFriendly.js';
 
 // Lägg till rubrik
 addMdToPage('## Average Age');
-addMdToPage('Following statistics show the average age of men and women, and the total average age of this survey..');
+addMdToPage('Following statistics show the average age of men and women, and the total average age of this survey.');
 let avgAge = await dbQuery("SELECT gender, ROUND(AVG(age), 2) AS average_age, CONCAT(ROUND(100.0 * SUM(depression) / COUNT(*), 2), '%') AS depression_percentage FROM results GROUP BY gender UNION ALL SELECT 'Total' AS gender, ROUND(AVG(age), 2) AS average_age, CONCAT(ROUND(100.0 * SUM(depression) / COUNT(*), 2), '%') AS depression_percentage FROM results; ")
 tableFromData({ data: avgAge })
 
