044104ba00a63920d40698b09ef0c7e210eec269
JonasLarsson-coder <Jonas.larsson.data24sto@edu.tucsweden.se>
2025-05-01 22:51:55 +0200

    text updated

--- js/_menu.js
+++ js/_menu.js
@@ -5,5 +5,6 @@ createMenu('Indian depression statistics', [
   { name: 'Average Age', script: 'avgAge.js' },
   { name: 'Suicidal', script: 'suicidalThoughtsAndDepression.js' },
   { name: 'Sleep', script: 'sleepAndDepressionDiagram.js' },
-  { name: 'Mentall Illnes', script: 'historyOfMentallIllnesAndDepression.js' }
+  { name: 'Mentall Illnes', script: 'historyOfMentallIllnesAndDepression.js' },
+  { name: 'test', script: 'test.js' },
 ]);
