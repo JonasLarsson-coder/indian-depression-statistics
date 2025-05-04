b13bdaedfac71163b0f9facaf3ea6a23e3a7a499
JonasLarsson-coder <Jonas.larsson.data24sto@edu.tucsweden.se>
2025-05-04 14:15:20 +0200

    Added text

---
+++ js/pearsonCorr.js
@@ -0,0 +1,29 @@
+
+addMdToPage(`
+**Tolkning av Pearson - korrelation.**
+
+Den Pearsons korrelationskoefficient(r) varierar från - 1 till 1och varje värde ger en inblick i hur och hur starkt sambandet mellan två variabler är. Att förstå dessa värden hjälper till att tolka korrelationens riktning och grad.
+
+  **Koefficientvärden:**
+
+**1:** Ett värde av + 1 anger en perfekt positivt linjärt samband mellan två variabler, vilket innebär att när den ena variabeln ökar, ökar den andra i perfekt proportion.
+
+- 1: Ett värde av - 1 anger en perfekt negativt linjärt sambanddär den ena variabeln ökar, medan den andra minskar i perfekt proportion.
+
+**0:** Ett värde av 0 föreslår inget linjärt samband mellan variablerna, vilket innebär att förändringar i den ena variabeln inte förutsäger förändringar i den andra.
+
+  **Positiva, negativa och nollkorrelationer:**
+
+**Positiv korrelation**: När r är positivt(t.ex. 0, 5) innebär det att båda variablerna tenderar att röra sig i samma riktning. När temperaturen stiger kan t.ex. glassförsäljningen öka, vilket visar på en positiv korrelation.
+
+**Negativ korrelation**: När r är negativt(t.ex. - 0, 7) tyder det på att variablerna rör sig i motsatt riktning. Ett exempel kan vara förhållandet mellan träningsfrekvens och kroppsfettprocent: när träningen ökar tenderar kroppsfettet att minska.
+
+**Noll korrelation**: En r av 0 innebär att det finns inget urskiljbart linjärt samband mellan variablerna. Det kan t.ex.saknas ett linjärt samband mellan skostorlek och intelligens.
+
+**I allmänhet**:
+
+0, 7 till 1 eller - 0, 7 till - 1 anger en stark korrelation.
+
+0, 3 till 0, 7 eller - 0, 3 till - 0, 7 återspeglar en måttlig korrelation.
+
+0 till 0, 3 eller - 0, 3 till 0 innebär en svag korrelation.`)
