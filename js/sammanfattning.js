286ba51a94f9184b4291c163b7a1668db3733a88
JonasLarsson-coder <Jonas.larsson.data24sto@edu.tucsweden.se>
2025-05-04 10:36:17 +0200

    text updated

--- js/sammanfattning.js
+++ js/sammanfattning.js
@@ -1,2 +1,22 @@
-addMdToPage('## Sammanfattning');
-addMdToPage('Denna sammanfattning ger en översikt över de viktigaste resultaten från undersökningen om psykisk ohälsa bland studenter i Indien. OBS EJ KLART ÄN.');
+
+
+addMdToPage(`
+
+## Självmordstankar  
+Pearsons korrelationskoefficient på 0.546 innebär en måttlig och positiv korrelation mellan självmordstankar och depression.  
+Det betyder att sambandet är synligt och signifikant, men det är inte extremt starkt.
+
+## Sömn  
+Studenter som sover mer än 5 timmar per natt har en lägre sannolikhet att uppleva depression jämfört med de som sover mindre än 5 timmar.
+
+## Familjehistorik av psykisk ohälsa  
+Resultatet av undersökningen visar att studenter som har psykisk ohälsa i familjen har en något högre sannolikhet att uppleva depression jämfört med de som inte har en sådan historia.  
+Pearsons korrelationskoefficient räknades ut men är statistiskt svag – för att korrelationen ska vara meningsfull krävs fler variationer i data.
+
+## Ekonomisk stress  
+Ett Pearson-korrelationsvärde på 0.36 innebär en måttlig positiv korrelation mellan ekonomisk stress och depression.  
+Det betyder att när finansiell stress ökar, tenderar sannolikheten för depression också att öka – men sambandet är inte starkt, vilket betyder att det finns andra faktorer som påverkar depression utöver finansiell stress.
+
+## Sammanfattning
+Undersökningen visar att ingen av ovanstående orsaker är avgörande för att förutsäga depression, utan snarare en av många faktorer som kan påverka en persons mentala hälsa.`);
+
